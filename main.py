41f200a803028cb3493efa28eb5177e5fc0cd961
attilacoderjam <32287233+attilacoder@users.noreply.github.com>
2022-01-15 21:57:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import json
+import requests
+from forever import keep_alive
+from urllib.request import urlopen
+import time
+class endpoints:
+    ac = "https://www.epicgames.com/id/logout?redirectUrl=https%3A%2F%2Fwww.epicgames.com%2Fid%2Flogin%3FredirectUrl%3Dhttps%253A%252F%252Fwww.epicgames.com%252Fid%252Fapi%252Fredirect%253FclientId%253Dec684b8c687f479fadea3cb2ad83f5c6%2526responseType%253Dcode"
+    token = "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token"
+    reward = "https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/{0}/client/ClaimLoginReward?profileId=campaign"
+
+def getToken(authCode: str):
+    h = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "basic ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ="
+    }
+    d = {
+            "grant_type": "authorization_code",
+            "code": authCode
+    }
+    r = requests.post(endpoints.token, headers=h, data=d)
+    print(r.text)
+    r = json.loads(r.text)
+    if "access_token" in r:
+        access_token = r["access_token"]
+        account_id= r["account_id"]
+        print(f"access_token: {access_token}\naccount_id: {account_id}\nexpires_at: {r['expires_at']}")
+        return access_token, account_id
+    else:
+        if "errorCode" in r:
+            print(f"[ERROR] {r['errorCode']}")
+        else:
+            print("[ERROR] Unknown error")
+        return False
+def getTokenFromLink(url: str):
+  # import json
+
+  # store the URL in url as 
+  # parameter for urlopen
+    
+  # store the response of URL
+  response = urlopen(url)
+    
+  # storing the JSON response 
+  # from url in data
+  data_json = json.loads(response.read())
+    
+  # print the json response
+  print(data_json)
+
+gtResult = getToken("e0e7792c5317466db631e8263c3503c5")
+keep_alive()
+while True:
+  if not gtResult:
+      exit()
+  else:
+      h = {
+              "Authorization": f"bearer {gtResult[0]}",
+              "Content-Type": "application/json"
+      }
+      r = requests.post(endpoints.reward.format(gtResult[1]), headers=h, data="{}")
+      #print(r.text)
+      print("Should be claimed. idk if you already claimed it")
+  time.sleep(86400)
+
